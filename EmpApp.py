7640e5eff1b0bfbb4b91ba4ce00986f3e522899e
Shruti Bharsakle <82164259+ShrutiBharsakle@users.noreply.github.com>
2025-09-18 03:23:28 +0530

    Update EmpApp.py
    Changed the port from 80 to 5000

--- EmpApp.py
+++ EmpApp.py
@@ -77,4 +77,5 @@ def success():
     return render_template('success.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+
+    app.run(debug=True, host='0.0.0.0', port=5000)
